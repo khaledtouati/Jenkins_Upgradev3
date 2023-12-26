6bcb95d5d8fcf27eb27bd3710d8b3107b839e6b3
khaledtouati <khaledtouatikhaled91@gmail.com>
2023-12-26 07:51:27 +0100

    dzezaazaa

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,10 +42,17 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
 
 
-        @Test
-  public void greetShouldIncludeGreetingMessage2() {
+    
+  }
+
+
+      @Test
+  public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+
+
+    
   }
 }
